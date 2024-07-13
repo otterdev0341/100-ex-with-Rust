61e5cc5b0a5dd102c49ac8dfe37a65f0bc61ed98
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2024-07-14 04:43:26 +0700

    done for today

---
+++ ex_rust_100/src/exc_6.rs
@@ -0,0 +1,14 @@
+/*
+    write a program that check if a string only contains numbers
+    if it does print True, Else print False
+
+    {input}     {output}
+    "Hello"     False
+    "4567"      True
+    "Hello59"   False
+    ""          False
+*/
+
+fn is_it_only_digits() -> bool {
+    
+}
--- ex_rust_100/target/debug/deps/module-fae6080d4af07878.d
+++ ex_rust_100/target/debug/deps/module-fae6080d4af07878.d
@@ -1,6 +1,6 @@
-/media/otterdev/OtterHeaven/CodingProject/100-ex-with-Rust/ex_rust_100/target/debug/deps/libmodule-fae6080d4af07878.rmeta: src/module.rs src/exc_1.rs src/exc_2.rs src/exc_3.rs src/exc_4.rs src/exc_5.rs
+/media/otterdev/OtterHeaven/CodingProject/100-ex-with-Rust/ex_rust_100/target/debug/deps/libmodule-fae6080d4af07878.rmeta: src/module.rs src/exc_1.rs src/exc_2.rs src/exc_3.rs src/exc_4.rs src/exc_5.rs src/exc_6.rs
 
-/media/otterdev/OtterHeaven/CodingProject/100-ex-with-Rust/ex_rust_100/target/debug/deps/module-fae6080d4af07878.d: src/module.rs src/exc_1.rs src/exc_2.rs src/exc_3.rs src/exc_4.rs src/exc_5.rs
+/media/otterdev/OtterHeaven/CodingProject/100-ex-with-Rust/ex_rust_100/target/debug/deps/module-fae6080d4af07878.d: src/module.rs src/exc_1.rs src/exc_2.rs src/exc_3.rs src/exc_4.rs src/exc_5.rs src/exc_6.rs
 
 src/module.rs:
 src/exc_1.rs:
@@ -8,3 +8,4 @@ src/exc_2.rs:
 src/exc_3.rs:
 src/exc_4.rs:
 src/exc_5.rs:
+src/exc_6.rs:
