0e6ff384e3fedd514d6b6a26daf95db172dd547c
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2024-07-17 21:10:20 +0700

    exc 44 almost work

--- ex_rust_100/src/exc_44.rs
+++ ex_rust_100/src/exc_44.rs
@@ -6,36 +6,103 @@ Each nested vec should contain a key as the first element and its corresponding 
 
 Print the final vec of vecs.
 
+Expected Output:
+If this is the original dictionary:
+
+product_info = {
+	"description": "shoe",
+	"price": 4.56,
+	"colors": ["green", "blue", "red"],
+}
+The output should be:
+
+[['description', 'shoe'], ['price', 4.56], ['colors', ['green', 'blue', 'red']]]
+
 */
+enum Case {
+    String,
+    Float,
+    VecString,
+    Default,
+}
+
+pub trait StringConvertible: Sized {
+    fn from_string(value: String) -> Self;
+    fn to_string(&self) -> String;
+}
+
+impl StringConvertible for String {
+    fn from_string(value: String) -> Self {
+        value
+    }
+
+    fn to_string(&self) -> String {
+        self.clone()
+    }
+}
+
+pub trait CastFromFloat: Sized {
+    fn cast_from_float(value: f64) -> Self;
+}
+
+impl CastFromFloat for f64 {
+    fn cast_from_float(value: f64) -> Self {
+        value
+    }
+}
 
 use std::{any::{self, Any}, collections::{btree_map::Values, HashMap}, fmt::{Debug, Display}};
 
 fn tranform_hash_to_vec<T>(data: HashMap<String, Box<dyn Any>>)
 where 
-    T : Any + Clone + Debug + Display + ToString,
+    T : Any + Clone + Debug + Display + StringConvertible + CastFromFloat,
 {
     let mut result_vec: Vec<T> = vec![];
-
+    let mut fn_case: Case = Case::Default;
     for (key, val) in data.into_iter() {
         //temp valut to insert with final extraction
         let mut key_vec: Vec<String> = vec![];
-        let mut value_vec: Vec<T> = vec![];
+        let mut value_vec_string: Vec<String> = vec![];
+        let mut value_vec_f64: Vec<f64> = vec![];
+        
         // key always be string cant insert in this process
-        key_vec.push(String::from(key));
+        result_vec.push(T::to_string(key));
 
         //check what kind of value that we got
         // String || number || vector
         if let Some(string_val) = val.downcast_ref::<String>() {
-            value_vec.push(string_val.to_owned());
-        } else if let Some(int_val) = val.downcast_ref::<f64>() {
             
+            value_vec_string.push(string_val.to_owned());
+            fn_case = Case::String
+
+        } else if let Some(int_val) = val.downcast_ref::<f64>() {
+
+            value_vec_f64.push(*int_val);
+            fn_case = Case::Float
+
         } else if let Some(vec_val) = val.downcast_ref::<Vec<String>>() {
             
+            value_vec_string.clear();
+            for data in vec_val {
+                value_vec_string.push(data.to_owned());
+            }
+            fn_case = Case::VecString
         } else {
             
         }
-        
-    }
+        match fn_case {
+            Case::String => {
+                result_vec.push(value_vec_string);
+            },
+            Case::Float => {
+                result_vec.push(T::cast_from_float(value_vec_f64[0]));
+            },
+            Case::VecString => {
+                println!("");
+            },
+            Case::Default => (),
+        }
+    }// end main for
 
     println!("{:?}",result_vec);
 }
@@ -52,6 +119,6 @@ mod test {
         context.insert("price".to_owned(), Box::new(4.56));
         context.insert("color".to_owned(), Box::new(vec!["green".to_owned(),"blue".to_owned(),"red".to_owned()]));
 
-        tranform_hash_to_vec::<String>(context);
+        tranform_hash_to_vec(context);
     }
 }
